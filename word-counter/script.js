89143ca84de9e12b16f71d1aacc2ac9dd5aba682
jeanetteong <jeanetteongjx@hotmail.com>
2023-08-20 22:51:55 +0800

    functional word counter

--- word-counter/script.js
+++ word-counter/script.js
@@ -1,10 +1,30 @@
 const input = document.getElementById('word-input');
+const words_number = document.getElementById('words');
+const characters_number = document.getElementById('characters');
 let words = 0;
 let characters = 0;
+words_number.innerHTML = words + ' words';
+characters_number.innerHTML = characters + ' characters';
 
 input.addEventListener('input', () => {     //everytime input updates
     let text = input.value;
     let text_array = text.split(" ");   //separates input by space
-    words = text_array.length;
+    if (text_array[text_array.length - 1] == '') {   //if theres only a space after the last word
+        words = text_array.length - 1;
+    } else {
+        words = text_array.length;
+    }
     characters = text.length;
+
+    if (words == 1) {       //if theres only one word
+        words_number.innerHTML = words + ' word';
+    } else {
+        words_number.innerHTML = words + ' words';
+    }
+
+    if (characters == 1) {      //if theres only 1 character
+        characters_number.innerHTML = characters + ' character';
+    } else {
+        characters_number.innerHTML = characters + ' characters';
+    }
 });
